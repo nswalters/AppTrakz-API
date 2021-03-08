6ebe9a6452ef28b126032e406c43a3f6a5b605bd
Nick Walters <nswalters.s@gmail.com>
2021-03-07 19:24:06 -0600

    Create an application; Enforce unique job application

--- apptrakzapi/models/application.py
+++ apptrakzapi/models/application.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import UniqueConstraint
 from django.contrib.auth.models import User
 
 
@@ -8,3 +9,7 @@ class Application(models.Model):
     job = models.ForeignKey("Job", on_delete=models.DO_NOTHING)
     submitted_at = models.DateTimeField(auto_now_add=True)
     is_active = models.BooleanField()
+
+    class Meta:
+        constraints = [UniqueConstraint(fields=['user', 'job'],
+                                        name="unique_job_application_user")]
