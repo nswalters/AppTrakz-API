09648ca9f8cc5ae1187ae6ba556bb143745a64d4
Nick Walters <nswalters.s@gmail.com>
2021-03-09 19:33:50 -0600

    Added update functionality for a company

--- apptrakzapi/views/company.py
+++ apptrakzapi/views/company.py
@@ -11,6 +11,33 @@ from apptrakzapi.models import Company
 class CompanyView(ViewSet):
     """ Company ViewSet """
 
+    def update(self, request, pk=None):
+        """
+        Handle PUT requests for a company
+        """
+        company = Company.objects.get(pk=pk)
+        company.name = request.data["name"]
+        company.address1 = request.data["address1"]
+
+        if "address2" in request.data:
+            company.address2 = request.data["address2"]
+
+        company.city = request.data["city"]
+        company.state = request.data["state"]
+        company.zipcode = request.data["zipcode"]
+        company.website = request.data["website"]
+
+        # Validate company data then serialize
+        try:
+            company.clean_fields()
+            company.save()
+            serializer = CompanySerializer(company, context={'request': None})
+
+            return Response(serializer.data)
+
+        except ValidationError as ex:
+            return Response(ex.args[0], status=status.HTTP_400_BAD_REQUEST)
+
     def create(self, request):
         """ Handle POST operations
 
