a038950d342469e1fea04a5f8c0fc9849e7944aa
Nick Walters <nswalters.s@gmail.com>
2021-03-13 15:00:44 -0600

    Create job contact works

--- apptrakzapi/views/job_contact.py
+++ apptrakzapi/views/job_contact.py
@@ -10,6 +10,40 @@ from apptrakzapi.models import Company, Contact, Job, JobContact
 class JobContactView(ViewSet):
     """ Job Contact ViewSet """
 
+    def create(self, request):
+        """
+        Handle POST requests to create a new job contact record
+        """
+
+        current_user = User.objects.get(pk=request.auth.user.id)
+
+        # Make sure the job we're adding the contact to belongs to the user
+        try:
+            job = Job.objects.get(pk=request.data['job'], user=current_user)
+        except Job.DoesNotExist as ex:
+            return Response({"reason": ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        contact = Contact()
+        contact.first_name = request.data['first_name']
+        contact.last_name = request.data['last_name']
+        contact.phone = request.data['phone']
+        contact.email = request.data['email']
+
+        job_contact = JobContact()
+        job_contact.contact = contact
+        job_contact.job = job
+
+        try:
+            contact.save()
+            job_contact.save()
+            serializer = JobContactSerializer(
+                job_contact, context={'request': None})
+
+            return Response(serializer.data)
+
+        except Exception as ex:
+            return Response(ex.args[0], status=status.HTTP_400_BAD_REQUEST)
+
     def update(self, request, pk=None):
         """
         Handle PUT requests for a job contact
