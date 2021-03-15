390f0d6ff9679ef0e405541bc3dfa45a955055d9
Nick Walters <nswalters.s@gmail.com>
2021-03-14 21:02:18 -0500

    Fixed bug due to using updated_at column which uses auto_now

--- apptrakzapi/models/application_status.py
+++ apptrakzapi/models/application_status.py
@@ -8,5 +8,6 @@ class ApplicationStatus(models.Model):
     application = models.ForeignKey(Application, on_delete=models.CASCADE)
     status = models.ForeignKey("Status", on_delete=models.DO_NOTHING)
     updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     reason = models.CharField(max_length=500, null=True, blank=True)
     is_current = models.BooleanField()
