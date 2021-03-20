09b92e797f102c75977240880a8af8721a47ba3e
Nick Walters <nswalters.s@gmail.com>
2021-03-20 15:23:41 -0500

    Configure DEBUG setting as environment variable

--- apptrakz/settings.py
+++ apptrakz/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+#DEBUG = False
+DEBUG = os.getenv('DEBUG', False)
 
 ALLOWED_HOSTS = []
 
